e1fc4d1fee3fda303de950f2254e11eea13cfd02
gemclarke1992 <Gemma Clarke>
2026-01-20 17:24:39 +0000

    buttons

--- apps.js
+++ apps.js
@@ -1,1 +1,16 @@
 console.log("hello");
+var seconds = 1000 * 60; //1000 = 1 second in JS
+var min = seconds * 60;
+document.getElementById("img").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  if (seconds >= 0) {
+    seconds = seconds--;
+  } else {
+    clearInterval("timer");
+    alert("You clicked X cookies!!!");
+  }
+  document.getElementById("timer").innerHTML = "0:" + seconds;
+}
